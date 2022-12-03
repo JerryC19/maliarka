746d0a335f55d4de359b2ba2d685b42a4558ff5d
Jerry Campos <jerrycampos02@gmail.com>
2022-12-03 17:18:36 -0600

    Funcionalidad de editar producto ADMIN

--- src/main/java/com/maliarka/controller/ProductoController.java
+++ src/main/java/com/maliarka/controller/ProductoController.java
@@ -7,11 +7,13 @@ package com.maliarka.controller;
 import com.maliarka.domain.Producto;
 import com.maliarka.domain.Usuario;
 import com.maliarka.service.ProductoService;
+import java.util.Optional;
 import org.slf4j.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -42,6 +44,21 @@ public class ProductoController {
         Usuario u= new Usuario(1,"","","","","","","");
         producto.setUsuario(u);
         productoService.save(producto);
+        return "redirect:/productos";
+    }
+    
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable Integer id, Model model){ //Es propio de Spring y sirve para mapear la variable que esta en el URl a la variable que esta a la par de PathVariable
+        Producto producto = new Producto();
+        Optional<Producto> optionalProducto=productoService.get(id);
+        producto= optionalProducto.get();
+        model.addAttribute("producto", producto);
+        return "productos/edit";
+    }
+    
+    @PostMapping("/update")
+    public String update(Producto producto){
+        productoService.update(producto);
         return "redirect:/productos";
     }
     
